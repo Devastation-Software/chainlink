37991083f16972bb67b1a81cdb678544079d41d1
William <67127399+thunder-red-star@users.noreply.github.com>
2022-02-28 06:57:02 +0000

    the code obviously won't work but the idea is there

--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,10 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-const sleep = (milliseconds) => {
-  return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
-
-function bytesToSize(bytes) {
-  var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-  if (bytes == 0) return "0 Bytes";
-  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
-}
-
 module.exports = async (client) => {
   client.user.setActivity("messages!", { type: "LISTENING" });
+
+	let botMessageChannel = client.channels.cache.get("947747031395467324")
+
+	botMessageChannel.send({ content: "I'm online! " + utils.time.fullTimestamp() })
 };
