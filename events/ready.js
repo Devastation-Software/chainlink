7cfca8b31ef50c862175f4c62300d029acb4a544
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-30 17:02:44 -0400

    loading emojis is a bust

--- events/ready.js
+++ events/ready.js
@@ -2,21 +2,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const chalk = require("chalk");
 const utils = require("../utils/utils.js");
-const emojis = require("../assets/emojis.js");
 
 module.exports = async (client) => {
-
-  // Load emojis
-  client.customEmojis = {};
-  // Get Emoji keys
-  const emojiKeys = Object.keys(emojis);
-  // Loop through emojis
-  emojiKeys.forEach(async (emoji) => {
-    // Get emoji data
-    const emojiData = emojis[emoji];
-    client.customEmojis[emoji] = await client.emojis.fetch(emojiData);
-  })
-
   client.user.setActivity("messages! Shard " + client.shardId, {
     type: "LISTENING",
   });
