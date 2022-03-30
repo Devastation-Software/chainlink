a5fe26439eed8fef546fe864bfbd45ff65b90252
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-30 19:20:09 -0400

    resolve file

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const DJSBuilders = require("@discordjs/builders");
 
 module.exports = async (client) => {
   // Check last checked version file, if it is not the same as the one in version.json, post a message to the change log channel and update the last checked version file
-  const lastCheckedVersion = fs.readFileSync("../data/lastcheckedversion", "utf8");
+  const lastCheckedVersion = fs.readFileSync("./data/lastcheckedversion", "utf8");
   if (!(lastCheckedVersion == client.info.build)) {
     const newReleaseEmbed = new DJSBuilders.Embed()
       .setTitle("New version released!")
@@ -16,7 +16,7 @@ module.exports = async (client) => {
       })
       .setTimestamp();
     client.channels.cache.get(changeLogChannelId).send({ embeds: [newReleaseEmbed] });
-    fs.writeFileSync("../data/lastcheckedversion", client.info.build);
+    fs.writeFileSync("./data/lastcheckedversion", client.info.build);
     console.log(`New version ${client.info.build}'s change log has been posted to the change log channel.`);
   }
 
