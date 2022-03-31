1d000991b3b2878c9737bd3b3af32bbba612f735
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-31 08:37:37 -0400

    extend scope of try/catch

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,51 +1,51 @@
-const Discord = require("discord.js"),
-  fs = require("fs");
+const Discord = require("discord.js"), fs = require("fs");
 
 module.exports = async (interaction) => {
-  let client = interaction.client;
-
-  if (!interaction.isCommand()) return;
-
-  const command = client.interactions.get(interaction.commandName);
-
-  if (!command) return;
-  const options = interaction.options;
-
-  command.perms.bot.forEach(perm => {
-    let clientMember = client.guilds.cache.get(interaction.guild.id).members.cache.get(client.user.id);
-    if (!clientMember.permissions.has(perm)) {
-      return interaction.reply("❌ I don't have the permission to do that! I need the  `" + perm + "` permission.");
-    }
-  });
-
-  // Owner only command check
-  if (command.perms.ownerOnly && ((interaction.user.id !== client.config.ownerID) && (client.config.testers.indexOf(interaction.user.id) === -1))) {
-    return interaction.reply("❌ That's an owner only command!");
-  }
-
-  // If the user is bot owner bypass all permission checks
-  if ((interaction.user.id !== client.config.ownerID) && (client.config.testers.indexOf(interaction.user.id) === -1)) {
-    // Do nothing, bypass all permission checks
-  } else {
-    command.perms.user.forEach(perm => {
-      if (!interaction.member.permissions.has(perm)) return interaction.reply("❌ You don't have the permission to do that!");
-    });
-  }
-
-  // If the interaction was already deferred edit reply instead of creating a new one
-  try {
-    await command.execute(interaction, client, options);
-  } catch (err) {
-    console.error(err);
-    let errorId = client.utils.errors.write(err)
     try {
-      await interaction.reply({
-        "content": "⚠️ An error occurred while executing that command! Please report this to the bot owner, with this error ID: `" + errorId + "`",
-      });
-    } catch {
-      await interaction.editReply({
-        "content": "⚠️ An error occurred while executing that command! Please report this to the bot owner, with this error ID: `" + errorId + "`",
-      });
+
+        let client = interaction.client;
+
+        if (!interaction.isCommand()) return;
+
+        const command = client.interactions.get(interaction.commandName);
+
+        if (!command) return;
+        const options = interaction.options;
+
+        command.perms.bot.forEach(perm => {
+            let clientMember = client.guilds.cache.get(interaction.guild.id).members.cache.get(client.user.id);
+            if (!clientMember.permissions.has(perm)) {
+                return interaction.reply("❌ I don't have the permission to do that! I need the  `" + perm + "` permission.");
+            }
+        });
+
+        // Owner only command check
+        if (command.perms.ownerOnly && ((interaction.user.id !== client.config.ownerID) && (client.config.testers.indexOf(interaction.user.id) === -1))) {
+            return interaction.reply("❌ That's an owner only command!");
+        }
+
+        // If the user is bot owner bypass all permission checks
+        if ((interaction.user.id !== client.config.ownerID) && (client.config.testers.indexOf(interaction.user.id) === -1)) {
+            // Do nothing, bypass all permission checks
+        } else {
+            command.perms.user.forEach(perm => {
+                if (!interaction.member.permissions.has(perm)) return interaction.reply("❌ You don't have the permission to do that!");
+            });
+        }
+
+        // If the interaction was already deferred edit reply instead of creating a new one
+        await command.execute(interaction, client, options);
+    } catch (err) {
+        console.error(err);
+        let errorId = client.utils.errors.write(err)
+        try {
+            await interaction.reply({
+                "content": "⚠️ An error occurred while executing that! Please report this to the bot owner, with this error ID: `" + errorId + "`",
+            });
+        } catch {
+            await interaction.editReply({
+                "content": "⚠️ An error occurred while executing that! Please report this to the bot owner, with this error ID: `" + errorId + "`",
+            });
+        }
     }
-  }
 };
