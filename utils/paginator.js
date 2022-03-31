1e68d4b1a65e1d9cd57eae1aba192853801b59ea
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-31 08:44:27 -0400

    debugging

--- utils/paginator.js
+++ utils/paginator.js
@@ -65,7 +65,7 @@ const paginationEmbed = async (interaction, pages, timeout = 120000) => {
     });
 
     collector.on("collect", async (i) => {
-        console.log(i.emoji.name);
+        console.log(i);
         switch (i.customId) {
             case buttonList[0].customId:
                 page = 0;
