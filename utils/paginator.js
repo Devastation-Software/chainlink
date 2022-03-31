0006c43000a3d9abf8c801d97a9c8219d6ef8070
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-31 08:47:18 -0400

    debugging

--- utils/paginator.js
+++ utils/paginator.js
@@ -12,22 +12,6 @@ const {
  */
 const paginationEmbed = async (interaction, pages, timeout = 120000) => {
     if (!pages) throw new Error("No pages for paginationEmbed!.");
-    buttonList = [new ButtonComponent()
-        .setCustomId("tobeginning")
-        .setStyle(1)
-        .setEmoji({name: "tobeginning", id: "947923816129130556", animated: false}), new ButtonComponent()
-        .setCustomId("last")
-        .setStyle(1)
-        .setEmoji({name: "arrowleft", id: "947923816062013471", animated: false}), new ButtonComponent()
-        .setCustomId("discard")
-        .setEmoji({name: "x_", id: "947923816120733747", animated: false})
-        .setStyle(4), new ButtonComponent()
-        .setCustomId("next")
-        .setEmoji({name: "arrowright", id: "947923816363982888", animated: false})
-        .setStyle(1), new ButtonComponent()
-        .setCustomId("toend")
-        .setEmoji({name: "toend", id: "947923816158494831", animated: false})
-        .setStyle(1),];
 
     let page = 0;
 
@@ -67,19 +51,19 @@ const paginationEmbed = async (interaction, pages, timeout = 120000) => {
     collector.on("collect", async (i) => {
         console.log(i.customId);
         switch (i.customId) {
-            case buttonList[0].customId:
+            case "tobeginning":
                 page = 0;
                 break;
-            case buttonList[1].customId:
+            case "last":
                 page = page > 0 ? --page : pages.length - 1;
                 break;
-            case buttonList[3].customId:
+            case "next":
                 page = page + 1 < pages.length ? ++page : 0;
                 break;
-            case buttonList[4].customId:
+            case "toend":
                 page = pages.length - 1;
                 break;
-            case buttonList[2].customId:
+            case "discard":
                 await interaction.deleteReply();
                 return;
             default:
