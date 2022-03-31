bdf92f26a628e8a6979f8790136e2c36eee6de52
ThunderRedStar <wvongphanith30@stuy.edu>
2022-03-31 08:35:48 -0400

    oh god oh

--- utils/paginator.js
+++ utils/paginator.js
@@ -1,7 +1,5 @@
 const {
-  Embed,
-    ButtonComponent,
-    ActionRow,
+    Embed, ButtonComponent, ActionRow,
 } = require("@discordjs/builders");
 
 /**
@@ -13,106 +11,98 @@ const {
  * @returns
  */
 const paginationEmbed = async (interaction, pages, timeout = 120000) => {
-  if (!pages) throw new Error("No pages for paginationEmbed!.");
+    if (!pages) throw new Error("No pages for paginationEmbed!.");
+    buttonList = [new ButtonComponent()
+        .setCustomId("tobeginning")
+        .setStyle(1)
+        .setEmoji({name: "tobeginning", id: "947923816129130556", animated: false}), new ButtonComponent()
+        .setCustomId("last")
+        .setStyle(1)
+        .setEmoji({name: "arrowleft", id: "947923816062013471", animated: false}), new ButtonComponent()
+        .setCustomId("discard")
+        .setEmoji({name: "x_", id: "947923816120733747", animated: false})
+        .setStyle(4), new ButtonComponent()
+        .setCustomId("next")
+        .setEmoji({name: "arrowright", id: "947923816363982888", animated: false})
+        .setStyle(1), new ButtonComponent()
+        .setCustomId("toend")
+        .setEmoji({name: "toend", id: "947923816158494831", animated: false})
+        .setStyle(1),];
 
-  let page = 0;
+    let page = 0;
 
-  const row = new ActionRow().addComponents(
-      new ButtonComponent()
-          .setCustomId("tobeginning")
-          .setStyle(1)
-          .setEmoji({ name: "tobeginning", id: "947923816129130556", animated: false }),
-      new ButtonComponent()
-          .setCustomId("last")
-          .setStyle(1)
-          .setEmoji({ name: "arrowleft", id: "947923816062013471", animated: false }),
-      new ButtonComponent()
-          .setCustomId("discard")
-          .setEmoji({ name: "x_", id: "947923816120733747", animated: false })
-          .setStyle(4),
-      new ButtonComponent()
-          .setCustomId("next")
-          .setEmoji({ name: "arrowright", id: "947923816363982888", animated: false })
-          .setStyle(1),
-      new ButtonComponent()
-          .setCustomId("toend")
-          .setEmoji({ name: "toend", id: "947923816158494831", animated: false })
-          .setStyle(1)
-  );
+    const row = new ActionRow().addComponents(new ButtonComponent()
+        .setCustomId("tobeginning")
+        .setStyle(1)
+        .setEmoji({name: "tobeginning", id: "947923816129130556", animated: false}), new ButtonComponent()
+        .setCustomId("last")
+        .setStyle(1)
+        .setEmoji({name: "arrowleft", id: "947923816062013471", animated: false}), new ButtonComponent()
+        .setCustomId("discard")
+        .setEmoji({name: "x_", id: "947923816120733747", animated: false})
+        .setStyle(4), new ButtonComponent()
+        .setCustomId("next")
+        .setEmoji({name: "arrowright", id: "947923816363982888", animated: false})
+        .setStyle(1), new ButtonComponent()
+        .setCustomId("toend")
+        .setEmoji({name: "toend", id: "947923816158494831", animated: false})
+        .setStyle(1));
 
-  //has the interaction already been deferred? If not, defer the reply.
+    //has the interaction already been deferred? If not, defer the reply.
 
-  if (interaction.deferred == false) {
-    await interaction.deferReply();
-  }
-
-  const curPage = await interaction.editReply({
-    content: `Page ${page + 1} / ${pages.length}`,
-    embeds: [pages[page]],
-    components: [row],
-    fetchReply: true,
-  });
-
-  const filter = (i) =>
-    i.customId === buttonList[0].customId ||
-    i.customId === buttonList[1].customId ||
-    i.customId === buttonList[2].customId ||
-    i.customId === buttonList[3].customId ||
-    i.customId === buttonList[4].customId;
-
-  const collector = await curPage.createMessageComponentCollector({
-    filter,
-    time: timeout,
-  });
-
-  collector.on("collect", async (i) => {
-    switch (i.customId) {
-      case buttonList[0].customId:
-        page = 0;
-        break;
-      case buttonList[1].customId:
-        page = page > 0 ? --page : pages.length - 1;
-        break;
-      case buttonList[3].customId:
-        page = page + 1 < pages.length ? ++page : 0;
-        break;
-      case buttonList[4].customId:
-        page = pages.length - 1;
-        break;
-      case buttonList[2].customId:
-        await interaction.deleteReply();
-        return;
-      default:
-        break;
+    if (interaction.deferred == false) {
+        await interaction.deferReply();
     }
-    await i.deferUpdate();
-    await i.editReply({
-      content: `Page ${page + 1} / ${pages.length}`,
-      embeds: [pages[page]],
-      components: [row],
-    });
-    collector.resetTimer();
-  });
 
-  collector.on("end", () => {
-    try {
-      if (!curPage.deleted) {
-        const disabledRow = new ActionRow().addComponents(
-          buttonList[0].setDisabled(true),
-          buttonList[1].setDisabled(true),
-          buttonList[2].setDisabled(true),
-          buttonList[3].setDisabled(true),
-          buttonList[4].setDisabled(true)
-        );
-        curPage.edit({
-          content: `Page ${page + 1} / ${pages.length}`,
-          embeds: [pages[page]],
-          components: [disabledRow],
+    const curPage = await interaction.editReply({
+        content: `Page ${page + 1} / ${pages.length}`, embeds: [pages[page]], components: [row], fetchReply: true,
+    });
+
+    const filter = (i) => i.customId === buttonList[0].customId || i.customId === buttonList[1].customId || i.customId === buttonList[2].customId || i.customId === buttonList[3].customId || i.customId === buttonList[4].customId;
+
+    const collector = await curPage.createMessageComponentCollector({
+        filter, time: timeout,
+    });
+
+    collector.on("collect", async (i) => {
+        switch (i.customId) {
+            case buttonList[0].customId:
+                page = 0;
+                break;
+            case buttonList[1].customId:
+                page = page > 0 ? --page : pages.length - 1;
+                break;
+            case buttonList[3].customId:
+                page = page + 1 < pages.length ? ++page : 0;
+                break;
+            case buttonList[4].customId:
+                page = pages.length - 1;
+                break;
+            case buttonList[2].customId:
+                await interaction.deleteReply();
+                return;
+            default:
+                break;
+        }
+        await i.deferUpdate();
+        await i.editReply({
+            content: `Page ${page + 1} / ${pages.length}`, embeds: [pages[page]], components: [row],
         });
-      }
-    } catch {}
-  });
-  return curPage;
+        collector.resetTimer();
+    });
+
+    collector.on("end", () => {
+        try {
+            if (!curPage.deleted) {
+                const disabledRow = new ActionRow().addComponents(buttonList[0].setDisabled(true), buttonList[1].setDisabled(true), buttonList[2].setDisabled(true), buttonList[3].setDisabled(true), buttonList[4].setDisabled(true));
+                curPage.edit({
+                    content: `Page ${page + 1} / ${pages.length}`, embeds: [pages[page]], components: [disabledRow],
+                });
+            }
+        } catch {
+        }
+    });
+    return curPage;
 };
 
 // usage: paginationEmbed(interaction, embeds, buttons, timeOut)
