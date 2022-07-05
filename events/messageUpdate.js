e3ef6f199dca67d4c36ff8db2841eb21953a01f6
ThunderRedStar <wvongphanith30@stuy.edu>
2022-07-05 18:16:47 -0400

    debug

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -7,28 +7,35 @@ module.exports = async (oldMessage, newMessage) => {
   let client = oldMessage.client;
   // Ignore webhooks so loops don't occur
   if (oldMessage.webhookID) return;
+  console.log("Message was not a webhook.");
   // Ignore bot's own messages
   if (oldMessage.author.id === client.user.id) return;
+  console.log("Message was not from the bot.");
   // Get id of message
   let messageID = oldMessage.id;
   // Now get the message from the database
   let otherMessage = client.utils.bridges.getMessageWebhookId(messageID);
+  console.log("Got message from database.");
   // If the message was not found, return
   if (!otherMessage) return;
   // Get the channel of the message
   let channel = client.channels.cache.get(otherMessage.channel);
+  console.log("Got channel.");
   // If the channel is not found, return
   if (!channel) return;
   // Now we know the ID of the webhook to edit. Get the webhook token representing the webhook in that channel.
   let webhookToken = client.utils.webhooks.getWebhook(channel.id);
+  console.log("Got webhook token.");
   // If the webhook token is not found, return
   if (!webhookToken) return;
   // Now we know the webhook token. Get the webhook from the client
   let webhook = client.webhooks.cache.get(webhookToken);
+  console.log("Got webhook.");
   // If the webhook is not found, return
   if (!webhook) return;
   // Get the other message
   let otherMessageObject = await channel.messages.fetch(otherMessage.message);
+  console.log("Got other message.");
   // Now we know the webhook. We'll attempt to edit the message in the other channel.
   let editedMessage = await webhook.editMessage(otherMessageObject, {
     content: newMessage.content,
@@ -41,4 +48,6 @@ module.exports = async (oldMessage, newMessage) => {
     }),
     components: newMessage.components
   });
+
+  console.log("Edited message.");
 }
