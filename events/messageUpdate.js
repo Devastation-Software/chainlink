b3bb57fb59d2c6af6f63ae18b96b727c21a4ba50
ThunderRedStar <wvongphanith30@stuy.edu>
2022-07-05 19:07:45 -0400

    bruh did i forget endpoints

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -16,6 +16,7 @@ module.exports = async (oldMessage, newMessage) => {
   // Get all the messages associated with the message ID
   let webhookMessages = client.utils.bridges.getMessageWebhookId(messageID);
   let bridges = client.utils.bridges.findBridgesByChannel(oldMessage.channel.id);
+  let endpointBridges = let bridges = client.utils.bridges.findBridgesByEndpoint(oldMessage.channel.id);
   // This should be an array, loop over each ID and edit the message
   for (let i = 0; i < bridges.length; i++) {
     console.log("Bridge number " + i + " of " + bridges.length);
@@ -25,6 +26,30 @@ module.exports = async (oldMessage, newMessage) => {
     let channel = await client.channels.fetch(bridge.endpoint);
     // Fetch the webhook info
     let webhook = await client.utils.webhooks.getWebhook(bridge.endpoint);
+    // Fetch the webhook given id and token
+    let webhookFetch = await client.fetchWebhook(webhook.id, webhook.token);
+    // Edit the message
+    let editedMessage = await webhookFetch.editMessage(webhookMessages[i], {
+      content: newMessage.content,
+      embeds: newMessage.embeds,
+      files: newMessage.attachments.map(attachment => {
+        return {
+          attachment: attachment.url,
+          name: attachment.filename
+        };
+      }),
+      components: newMessage.components
+    });
+  }
+
+  for (let i = 0; i < endpointBridges.length; i++) {
+    console.log("Bridge number " + i + " of " + endpointBridges.length);
+    // Get the bridge info
+    let bridge = endpointBridges[i];
+    // Get the channel info
+    let channel = await client.channels.fetch(bridge.channel);
+    // Fetch the webhook info
+    let webhook = await client.utils.webhooks.getWebhook(bridge.channel);
     // Fetch the webhook given id and token
     let webhookFetch = await client.fetchWebhook(webhook.id, webhook.token);
     // Edit the message
